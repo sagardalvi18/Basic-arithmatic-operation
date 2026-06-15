54965bc38269e2dc48eb691227cec7a3ec115fc6
sagardalvi18 <sagardalvi2122@gmail.com>
2026-06-15 14:01:47 +0530

    Add files via upload
    Arithmatic operation

---
+++ addition.c
@@ -0,0 +1,25 @@
+// Sagar Ravindra Dalvi
+//PRN- 25030331113722503033111372L000
+#include<stdio.h>
+int main()
+{
+	int num1,num2,sum,sub,mul,div;
+	printf("Enter number1:\t");
+	scanf("%d",&num1);
+	printf("Enter number2:\t");
+	scanf("%d",&num2);
+	sum=num1+num2;
+	printf("addition of %d & %d is\t %d",
+	num1,num2,sum);
+	sub=num1-num2;
+	printf("\nsubtraction of %d & %d is\t %d",
+	num1,num2,sub);
+	mul=num1*num2;
+	printf("\nmultiplication of %d & %d is\t %d",
+	num1,num2,mul);
+	div=num1/num2;
+	printf("\ndivision of %d & %d is\t %d",
+	num1,num2,div);
+	return 0;
+	}
+   	  
